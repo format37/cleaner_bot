42840c1ac128903b4c006a87f2b1c2ae8c20562c
Alex <format37@gmail.com>
2020-09-26 09:40:06 +0300

    modified:   cleaner_bot_script.py

--- cleaner_bot_script.py
+++ cleaner_bot_script.py
@@ -1,2 +1,20 @@
+def cleaner_bot_init(WEBHOOK_HOST,WEBHOOK_PORT,WEBHOOK_SSL_CERT, SCRIPT_PATH):
+
+	with open(SCRIPT_PATH+'token.key','r') as file:
+		API_TOKEN=file.read().replace('\n', '')
+		file.close()
+	bot = telebot.TeleBot(API_TOKEN)
+
+	WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+	WEBHOOK_URL_PATH = "/{}/".format(API_TOKEN)
+
+	# Remove webhook, it fails sometimes the set if there is a previous webhook
+	bot.remove_webhook()
+
+	# Set webhook
+	wh_res = bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,certificate=open(WEBHOOK_SSL_CERT, 'r'))	
+
+	return bot
+
 def cleaner_bot_unauthorized():
 	return 'unauthorized'
