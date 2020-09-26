558c2e91ad99c9e8bb4d54e70d3fa265efa11c9e
Alex <format37@gmail.com>
2020-09-26 21:10:48 +0300

    modified:   cleaner_bot_script.py 	modified:   data.csv 	modified:   stats.ipynb

--- cleaner_bot_script.py
+++ cleaner_bot_script.py
@@ -1,14 +1,19 @@
 import pandas as pd
+import seaborn as sns
 	
 def cleaner_bot_unauthorized():
 	return 'unauthorized'
 
 def cleaner_bot_stats():
-	df = pd.read_csv('data.csv')
-	df = df.drop(['accounts'],axis = 1)
-	df = df.set_index('Пользователи')
+	df_full = pd.read_csv('data.csv')
+	df = pd.DataFrame()
+	df['user']=df_full['user']
+	df['Посуда']=df_full['Посуда']
+	df['Мусор']=df_full['Мусор']
+	df['Туалет']=df_full['Туалет']
+	df = df.set_index('user')
 	heat_map = sns.heatmap(df, annot=True, cmap="YlGnBu")
-	fig = heat_map.get_figure()
+	fig = heat_map.get_figure()	
 	filepath = "/home/format37_gmail_com/projects/cleaner_bot/images/heat_map.png"
 	fig.savefig(filepath)
 	return filepath
