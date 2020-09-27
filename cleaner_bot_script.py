a57a807ac252cd2c7f25a70feb82c0b30c6f4c46
Alex <format37@gmail.com>
2020-09-27 08:39:45 +0300

    modified:   cleaner_bot_script.py

--- cleaner_bot_script.py
+++ cleaner_bot_script.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import seaborn as sns
 
-def cleaner_bot_counter_plus(user_id,script_path,task):
+def cleaner_bot_counter_plus(account_id,script_path,task):
 	df_full = pd.read_csv(script_path+'data.csv')
 	current_value = int(df_full[df_full.account_id==account_id][task])
 	df_full.loc[(df_full.account_id==account_id),task]=current_value+1
 	df_full.to_csv('data.csv',index = None)
 	return task+' +1'
 
-def cleaner_bot_alert(user_id,script_path,task):	
+def cleaner_bot_alert(script_path,task):	
 	df_full				= pd.read_csv(script_path+'data.csv')
 	df					= pd.DataFrame()
 	df['user']			= df_full['user']
@@ -37,6 +37,6 @@ def cleaner_bot_stat(script_path):
 	fig.savefig(filepath)
 	return filepath
 
-def cleaner_bot_user_authorized(user_id,script_path):
+def cleaner_bot_user_authorized(account_id,script_path):
 	df_full = pd.read_csv(script_path+'data.csv')		
-	return str(user_id) in [str(i) for i in df_full.account_id.to_list()]
+	return str(account_id) in [str(i) for i in df_full.account_id.to_list()]
