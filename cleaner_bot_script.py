53a290fdd4152edec089c17b2e54aa994afca40f
Alexey Yurasov iceberg <format37@gmail.com>
2021-09-09 05:54:06 +0000

    modified:   cleaner_bot_script.py 	modified:   data.csv 	modified:   images/heat_map.png

--- cleaner_bot_script.py
+++ cleaner_bot_script.py
@@ -5,9 +5,20 @@ import matplotlib.pyplot as plt
 def cleaner_bot_counter_plus(account_id,script_path,task):
 	df_full = pd.read_csv(script_path+'data.csv')
 	current_value = int(df_full[df_full.account_id==account_id][task])
-	df_full.loc[(df_full.account_id==account_id),task]=current_value+1
+	df_task = df_full[['account_id', task]]
+
+	# Economics ++
+	account_value = float(df_task[df_task.account_id==account_id][task])
+	appendix = 1
+	if len(df_task)>1 and account_value>df_task[task].min():
+		for idx,row in df_task.iterrows():
+			if account_value>row[task]:
+				appendix += (account_value-row[task])/10
+	# Economics --
+
+	df_full.loc[(df_full.account_id==account_id),task]=current_value+round(appendix,1)
 	df_full.to_csv(script_path+'data.csv',index = None)
-	return task+' +1'
+	return task+' +'+str(round(appendix,1))
 
 def cleaner_bot_counter_minus(account_id,script_path,task):	
 	df_full = pd.read_csv(script_path+'data.csv')
@@ -43,9 +54,9 @@ def cleaner_bot_stat(script_path):
 	df['garbage']	= df_full['garbage']
 	df['toilet']	= df_full['toilet']
 	df['dry']		= df_full['dry']
-	df 				= df.set_index('user')		
+	df 				= df.set_index('user')
 	fig 			= plt.figure() # clean
-	heat_map		= sns.heatmap(df, annot=True, cmap="pink", cbar = False)
+	heat_map		= sns.heatmap(df, annot=True, cmap="pink", cbar = False, fmt=".1f")
 	fig 			= heat_map.get_figure()	
 	image_path = script_path+'images/heat_map.png'	
 	fig.savefig(image_path)
